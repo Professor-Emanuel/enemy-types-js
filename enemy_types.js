933abffba190aa59d8a18e4e495147463f910804
Professor-Emanuel <87651738+Professor-Emanuel@users.noreply.github.com>
2021-09-30 22:59:41 +0300

    Add files via upload
    my deltaTime in class Enemy is undefined, which is a problem since I want to use it to make enemies move at different speeds, but still the speed not be machine dependent

--- enemy_types.js
+++ enemy_types.js
@@ -14,10 +14,12 @@ document.addEventListener('DOMContentLoaded', function(){
             this.enemies = [];
             this.enemyInterval = 1000; // 1000 milliseconds
             this.enemyTimer = 0;
-            this.#addNewEnemy();
+            this.enemyTypes = ['worm', 'ghost'];
         }
     
         update(deltaTime){
+            //only take objects that are not market for deletion
+            this.enemies = this.enemies.filter(object => !object.markedForDeletion);
             if(this.enemyTimer > this.enemyInterval){
                 this.#addNewEnemy();
                 this.enemyTimer = 0;
@@ -35,26 +37,70 @@ document.addEventListener('DOMContentLoaded', function(){
         // # = private method
         // meaning we can call it only inside the class
         #addNewEnemy(){
-            this.enemies.push(new Enemy(this));
+            const randomEnemy = this.enemyTypes[Math.floor(Math.random() * this.enemyTypes.length)];
+            if(randomEnemy == 'worm'){
+                this.enemies.push(new Worm(this));
+            }
+            if(randomEnemy == 'ghost'){
+                this.enemies.push(new Ghost(this));
+            }
+            /*this.enemies.sort(function(a, b){
+                return a.y - b.y;
+            });*/
         }
     }
     
     class Enemy{
         constructor(game){
             this.game = game;
-            console.log(game);
-            this.x = this.game.width;
-            this.y = Math.random() * this.game.height;
-            this.width = 100;
-            this.height = 100;
+            //console.log(game);  
+            this.markedForDeletion = false;
         }
     
-        update(){
-            this.x--;
+        update(deltaTime){
+            this.x -= this.vx; //* deltaTime;
+            //console.log(deltaTime);// -> UNDEFINED for some reason
+            //console.log(this.vx);
+            if(this.x < 0 - this.width){
+                this.markedForDeletion = true;
+            }
         }
     
         draw(ctx){
-            ctx.fillRect(this.x, this.y, this.width, this.height);
+            //ctx.fillRect(this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, 0 , 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+        }
+    }
+
+    class Worm extends Enemy{
+        constructor(game){
+            //take the parent constructor properties
+            super(game);
+            //additional, specific child class properties
+            this.spriteWidth = 229; //see image
+            this.spriteHeight = 171; //see image
+            this.width = this.spriteWidth/2;
+            this.height = this.spriteHeight/2;
+            this.x = this.game.width;
+            this.y = this.game.height - this.height;
+            this.image = worm; // worm is the id of one of the image in index.html
+            this.vx = Math.random() * 0.2 + 0.1;
+        }
+    }
+
+    class Ghost extends Enemy{
+        constructor(game){
+            //take the parent constructor properties
+            super(game);
+            //additional, specific child class properties
+            this.spriteWidth = 229; //see image
+            this.spriteHeight = 171; //see image
+            this.width = this.spriteWidth/2;
+            this.height = this.spriteHeight/2;
+            this.x = this.game.width;
+            this.y = Math.random() * this.game.height * 0.6;
+            this.image = ghost; // worm is the id of one of the image in index.html
+            this.vx = Math.random() *  0.2 + 0.1;
         }
     }
 
@@ -70,6 +116,5 @@ document.addEventListener('DOMContentLoaded', function(){
         requestAnimationFrame(animate);
     };
     //call function
-    animate(0);
-    
+    animate(0); 
 });
